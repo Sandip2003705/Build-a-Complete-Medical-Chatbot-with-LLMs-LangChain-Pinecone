64c20a65d7b80984a4f1b346a167d0aa3bbba967
Sandip Verma <sandipverma3437@gmail.com>
2026-05-10 19:21:13 +0530

    folder structure added

---
+++ template.sh
@@ -0,0 +1,17 @@
+# Creatung directory
+mkdir -p src
+mkdir -p research
+
+# Creating files
+touch src/__init__.py
+touch src/.py
+touch src/prompt.py
+touch .env
+touch setup.py
+touch app.py
+touch research/trials.ipynb
+touch requirements.txt
+
+
+
+echo "Directory and files created sucessfully!."
