0f2f3a3f394f1dfe21ba4da18c57e620ce9da45d
Sandip Verma <sandipverma3437@gmail.com>
2026-05-11 23:50:02 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot_egg_info",
+    version="0.1.0",
+    author="Sandip Verma",
+    author_email="sandipverma3437@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
